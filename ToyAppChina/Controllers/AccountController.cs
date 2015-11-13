adf85caff79ab56897bbd53fb6b583a7899174d3
Pavel Chuchuva <Pavel.Chuchuva@software.dell.com>
2015-11-13 17:44:14 +1100

    Add signup

---
+++ ToyAppChina/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using ToyAppChina.Models;
+
+namespace ToyAppChina.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("account/signup")]
+        public ActionResult Signup()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("account/signup")]
+        public async Task<ActionResult> Signup(SignupViewModel model)
+        {
+            var user = new ApplicationUser
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FullName = model.FullName
+            };
+
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            var result = await userManager.CreateAsync(user, model.Password);
+            return Json(result);
+        }
+    }
+}
--- ToyAppChina/Models/ApplicationDbContext.cs
+++ ToyAppChina/Models/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -6,12 +7,17 @@ using System.Web;
 
 namespace ToyAppChina.Models
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext() : base("DefaultConnection")
         {
         }
 
         public DbSet<Movie> Movies { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
     }
 }
