d2b08d81eef4069082b2cf01c788c71f991c2c81
Pavel Chuchuva <Pavel.Chuchuva@software.dell.com>
2015-11-27 17:24:05 +1100

    Add debug action
    Don't forget to remove later!

--- ToyAppChina/Controllers/StaticPagesController.cs
+++ ToyAppChina/Controllers/StaticPagesController.cs
@@ -32,5 +32,14 @@ namespace ToyAppChina.Controllers
         {
             return View();
         }
+
+        [Route("debug-connection-string-hjkdfh484323rf")]
+        public ActionResult DebugConnectionStringDontForgetToDelete()
+        {
+            var connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"];
+            return Content((connectionString != null ? connectionString.ConnectionString : "null :(") +
+                System.Configuration.ConfigurationManager.AppSettings["SQLSERVER_URI"]);
+        }
+
     }
 }
